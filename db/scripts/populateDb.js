8f0d4d1cfedf38372db0f3586e727fe80c7e94f8
guudewie <philippthiessen12@gmail.com>
2024-09-26 21:13:59 +0200

    add populate db script

---
+++ db/scripts/populateDb.js
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+
+require("dotenv").config();
+const { Client } = require("pg");
+
+const SQL = `
+
+CREATE TYPE membership_status AS ENUM ('normal', 'member', 'admin');
+
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  username VARCHAR(20),
+  pass_hash TEXT,
+  status membership_status,
+  created_at timestamp DEFAULT current_timestamp
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title TEXT,
+  message TEXT,
+  user_id INTEGER,
+  created_at timestamp DEFAULT current_timestamp,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+`;
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    connectionString: process.env.DATABASE_LOCAL_URL,
+  });
+  await client.connect();
+  console.log("connecting...");
+  await client.query(SQL);
+  console.log("executing...");
+  await client.end();
+  console.log("done");
+}
+
+main();

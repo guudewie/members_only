aed406770bfaaa78426b57f9cd1a582beacc2ba7
guudewie <philippthiessen12@gmail.com>
2024-09-29 11:26:16 +0200

    add custom color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./views/**/*.ejs"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#8ecae6",
+          100: "219ebc",
+          200: "023047",
+        },
+        secondary: {
+          50: "#ffb703",
+          100: "#fb8500",
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };

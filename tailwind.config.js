3b6d117cde57327cc858f855e28732b6c4da82ea
guudewie <philippthiessen12@gmail.com>
2024-09-29 11:28:04 +0200

    fix color codes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,8 @@ module.exports = {
       colors: {
         primary: {
           50: "#8ecae6",
-          100: "219ebc",
-          200: "023047",
+          100: "#219ebc",
+          200: "#023047",
         },
         secondary: {
           50: "#ffb703",

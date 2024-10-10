51ae237e32d38cc32f40e45f608530cc708ab48b
guudewie <philippthiessen12@gmail.com>
2024-10-10 16:29:22 +0200

    set up passport local strategy

---
+++ config/passport/passport.js
@@ -0,0 +1,30 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
+const Pool = require("./db/pool");
+const db = require("../../db/queries/queries");
+const bcrypt = require("bcrypt");
+
+const verifyCallback = async (username, password, done) => {
+  try {
+    const user = db.getUser(username);
+
+    if (!user) {
+      return done(null, false, { message: "Incorrect username" });
+    }
+
+    const match = await bcrypt.compare(password, user.passport);
+
+    if (match) {
+      return done(null, user);
+    } else {
+      return done(null, false, { message: "Incorrect password" });
+    }
+  } catch (err) {
+    return done(err);
+  }
+};
+
+const strategy = new LocalStrategy(verifyCallback);
+
+passport.use(strategy);

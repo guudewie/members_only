1d5becb3ade908a040fb977ef4728880f05828a6
guudewie <philippthiessen12@gmail.com>
2024-10-12 13:38:12 +0200

    organize app.js

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
-const express = require("express");
-const expressLayouts = require("express-ejs-layouts");
-const app = express();
-const path = require("node:path");
-
-const indexRouter = require("./routes/index");
-
 require("dotenv").config();
 
-// set upt view engine ejs
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const path = require("node:path");
+const expressSession = require("./config/session");
+const passport = require("passport");
+const indexRouter = require("./routes/index");
+
+const app = express();
+const PORT = 3000;
+
+// Passport configuration
+require("./config/passport");
+
+// view engine setup
 app.use(expressLayouts);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// Middleware
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(expressSession);
+app.use(passport.session());
 
-// authentication
-require("./config/passport/passport");
-app.use;
+// Custom Middleware
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
 
+// Routes
 app.use("/", indexRouter);
 
-const PORT = 3000;
-
+// Start server
 app.listen(PORT, () => console.log(`Running... listening on Port ${PORT}`));
